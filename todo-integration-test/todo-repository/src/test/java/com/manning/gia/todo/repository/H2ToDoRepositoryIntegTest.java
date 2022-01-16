f42a75963891c802d32c27f35aa2299f579e2c5b
devalone <devalone@126.com>
2022-01-16 11:04:52 +0800

    添加 todo-integration-test 多项目构建，通过在 todo.java-convention 插件中增加 integrationTest sourceSet 加入集成测试支持，并添加 groovy 插件，在所有应用此插件的子项目中，通过简单的配置，进行集成测试。

---
+++ todo-integration-test/todo-repository/src/test/java/com/manning/gia/todo/repository/H2ToDoRepositoryIntegTest.java
@@ -0,0 +1,30 @@
+package com.manning.gia.todo.repository;
+
+import com.manning.gia.todo.model.ToDoItem;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class H2ToDoRepositoryIntegTest {
+    private ToDoRepository h2ToDoRepository;
+
+    @Before
+    public void setUp() {
+        h2ToDoRepository = new H2ToDoRepository();
+    }
+
+    @Test
+    public void testInsertToDoItem() {
+        ToDoItem newToDoItem = new ToDoItem();
+        newToDoItem.setName("Write integration tests");
+        Long newId = h2ToDoRepository.insert(newToDoItem);
+        newToDoItem.setId(newId);
+        assertNotNull(newId);
+
+        ToDoItem persistedToDoItem = h2ToDoRepository.findById(newId);
+        assertNotNull(persistedToDoItem);
+        assertEquals(newToDoItem, persistedToDoItem);
+    }
+}
